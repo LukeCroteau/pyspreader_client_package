cdefef7ac415962a3242ccbf03c3e3a2cad8573d
Luke Croteau <luke.j.croteau@gmail.com>
2019-05-13 12:15:43 -0400

    Rename Demo Classes

--- demo_client.py
+++ demo_client.py
@@ -7,13 +7,15 @@ from pyspreader.client import SpreadClient, MSSQLSpreadClient
 load_dotenv(verbose=True)
 
 if __name__ in ('__main__', 'test_client'):
-    cli = SpreadClient(connection_string=os.environ.get('SPREADER_LIVE_PG_DSN'), debug=True)
+    '''
+    cli = SpreadClient(connection_string=os.environ.get('SPREADER_TEST_PG_DSN'), debug=True)
     cli.agent_name = 'Test Agent'
     agentid = cli.connect()
 
     print('Current PG Agent ID is', agentid)
+    '''
 
-    cliMS = MSSQLSpreadClient(connection_string=os.environ.get('SPREADER_LIVE_MS_DSN'), debug=True)
+    cliMS = MSSQLSpreadClient(connection_string=os.environ.get('SPREADER_TEST_MS_DSN'), debug=True)
     cliMS.agent_name = 'Test Agent'
     agentid = cliMS.connect()
 
